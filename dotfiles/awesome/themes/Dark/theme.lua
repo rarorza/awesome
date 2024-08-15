8771233510fbd5b59db0802c0de744cea867ffd5
Rafael Rosa <79066006+rarorza@users.noreply.github.com>
2024-08-15 13:27:27 -0300

    Update theme.lua

--- dotfiles/awesome/themes/Dark/theme.lua
+++ dotfiles/awesome/themes/Dark/theme.lua
@@ -28,7 +28,8 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
 theme.border_normal = "#111111"
-theme.border_focus  = "#666666"
+theme.border_focus  = "#5c7e9a"
+--theme.border_focus  = "#666666"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
