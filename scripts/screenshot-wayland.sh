6404b32ebc96347e60f75137b30b138c0a5267ce
Rafael R Rosa <rarorza@proton.me>
2024-08-17 13:24:49 -0300

    update

--- scripts/screenshot-wayland.sh
+++ scripts/screenshot-wayland.sh
@@ -3,3 +3,8 @@
 folder=$HOME"/Pictures/Screenshots"
 
 grim -g "$(slurp)" "$folder"/"$(date +%Y%m%d-%H%M%S)".png
+
+recent_screenshot=$(ls -t "$folder" | head -n 1)
+complete_path_file="$folder/$recent_screenshot"
+wl-copy < "$complete_path_file"
+
