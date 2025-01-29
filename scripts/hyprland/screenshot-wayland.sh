33f08ed57b5ff7d7df6023a0004d9a5c70d5df84
Rafael R Rosa <79066006+rarorza@users.noreply.github.com>
2025-01-29 20:14:35 -0300

    Update screenshot-wayland.sh

--- scripts/hyprland/screenshot-wayland.sh
+++ scripts/hyprland/screenshot-wayland.sh
@@ -2,6 +2,10 @@
 
 folder=$HOME"/Pictures/Screenshots"
 
+if ! [ -d folder ]; then
+  mkdir ~/Pictures/Screenshots
+fi
+
 grim -g "$(slurp)" "$folder"/"$(date +%Y%m%d-%H%M%S)".png
 
 recent_screenshot=$(ls -t "$folder" | head -n 1)
